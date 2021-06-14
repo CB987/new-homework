60440b3b77bd7f278911c06a641937604bd26989
clare <grimalkinclare@gmail.com>
2021-06-13 22:36:03 -0400

    sets up structure of routes for site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get "/home", to: "home#index"
   get "/staff", to: "staff#index"
+  get "/events", to: "events#index"
+  get "/exhibitions", to: "exhibitions#index"
+  get "/news", to: "news#index"
+  get "/services", to: "services#index"
+  get "/usinglibrary", to: "usinglibrary#index"
+  get "/tools", to: "tools#index"
+  get "/materials", to: "materials#index"
+  get "/catalog", to: "catalog#index"
+  get "/database", to: "database#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
