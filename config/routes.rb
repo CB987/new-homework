7d921800fd5fcd45a51d37d2c6e38dd8a8dd19a3
clare <grimalkinclare@gmail.com>
2021-06-14 07:51:12 -0400

    adds fonts and placeholder titles for index pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root "home#index"
-  get "/home", to: "home#index"
-  get "/staff", to: "staff#index"
-  get "/events", to: "events#index"
-  get "/exhibitions", to: "exhibitions#index"
+  resources :home
+  # get "/home", to: "home#index"
+  resources :staff
+  # get "/staff", to: "staff#index"
+  resources :events 
+  # get "/events", to: "events#index"
+  resources :exhibitions
+  # get "/exhibitions", to: "exhibitions#index"
   get "/news", to: "news#index"
   get "/services", to: "services#index"
   get "/usinglibrary", to: "usinglibrary#index"
